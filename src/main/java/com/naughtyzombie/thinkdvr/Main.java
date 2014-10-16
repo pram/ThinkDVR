e27d11f3c282502ccbc281bf93ad0778e5dd9b7a
Pram Attale <pram@attale.com>
2014-10-16 10:50:31 +0100

    Adding twitter4j client

--- src/main/java/com/naughtyzombie/thinkdvr/Main.java
+++ src/main/java/com/naughtyzombie/thinkdvr/Main.java
@@ -1,11 +1,123 @@
 package com.naughtyzombie.thinkdvr;
 
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.BasicClient;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import com.twitter.joauth.UnpackedRequest;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by pattale on 16/10/2014.
  */
 public class Main {
+
+    private static AccessToken accessToken;
+
+    public static void run(String consumerKey, String consumerSecret, String token, String secret) throws InterruptedException {
+        // Create an appropriately sized blocking queue
+        BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+
+        // Define our endpoint: By default, delimited=length is set (we need this for our processor)
+        // and stall warnings are on.
+        StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
+        endpoint.stallWarnings(false);
+
+        Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        //Authentication auth = new com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
+
+        // Create a new BasicClient. By default gzip is enabled.
+        BasicClient client = new ClientBuilder()
+                .name("ThinkDVR")
+                .hosts(Constants.STREAM_HOST)
+                .endpoint(endpoint)
+                .authentication(auth)
+                .processor(new StringDelimitedProcessor(queue))
+                .build();
+
+        // Establish a connection
+        client.connect();
+
+        // Do whatever needs to be done with messages
+        for (int msgRead = 0; msgRead < 1000; msgRead++) {
+            if (client.isDone()) {
+                System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
+                break;
+            }
+
+            String msg = queue.poll(5, TimeUnit.SECONDS);
+            if (msg == null) {
+                System.out.println("Did not receive a message in 5 seconds");
+            } else {
+                System.out.println(msg);
+            }
+        }
+
+        client.stop();
+
+        // Print some stats
+        System.out.printf("The client read %d messages!\n", client.getStatsTracker().getNumMessages());
+    }
+
+    private void testRun(String consumerKey, String consumerSecret) throws TwitterException, IOException {
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer(consumerKey, consumerSecret);
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            String pin = br.readLine();
+            try{
+                if(pin.length() > 0){
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                }else{
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if(401 == te.getStatusCode()){
+                    System.out.println("Unable to get the access token.");
+                }else{
+                    te.printStackTrace();
+                }
+            }
+        }
+        //persist to the accessToken for future reference.
+        storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
+        Status status = twitter.updateStatus("Update");
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        System.exit(0);
+    }
+    private void storeAccessToken(long useId, AccessToken accessToken){
+        this.accessToken = accessToken;
+    }
+
+
     public static void main(String[] args) {
         Main main = new Main();
-        System.out.println("Main start = " + 1 );
+        System.out.println("Main start");
+        //main.run(args[0], args[1], args[2], args[3]);
+        //main.testRun(args[0], args[1]);
+
+
     }
 }
