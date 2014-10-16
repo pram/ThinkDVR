e270c7eaa654628845d8901a034df3e8a9077018
Pram Attale <pram@attale.com>
2014-10-16 09:09:16 +0100

    Adding Main

---
+++ src/main/java/com/naughtyzombie/thinkdvr/Main.java
@@ -0,0 +1,11 @@
+package com.naughtyzombie.thinkdvr;
+
+/**
+ * Created by pattale on 16/10/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println("Main start = " + 1 );
+    }
+}
