08f8eba790b11360ef31f4a7e2cfc2cd300189bb
Pram Attale <pram@attale.com>
2014-10-19 22:58:37 +0100

    removing some commented code

--- src/main/java/com/naughtyzombie/thinkdvr/Main.java
+++ src/main/java/com/naughtyzombie/thinkdvr/Main.java
@@ -115,71 +115,12 @@ public class Main {
 
         twitter.setOAuthAccessToken(accessToken);
         System.out.println(twitter.getScreenName());
-        //Status status = twitter.updateStatus("Update");
-        //System.out.println("Successfully updated the status to [" + status.getText() + "].");
         System.exit(0);
     }
 
     private void storeAccessToken(long useId, AccessToken accessToken) throws IOException {
         this.accessToken = accessToken;
         writeObject(accessToken);
-    }
-
-    private void run2(String consumerKey, String consumerSecret) {
-        // If you choose to use a callback, "oauth_verifier" will be the return value by Twitter (request param)
-        OAuthService service = new ServiceBuilder()
-                .provider(TwitterApi.class)
-                .apiKey(consumerKey)
-                .apiSecret(consumerSecret)
-                .build();
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("=== Twitter's OAuth Workflow ===");
-        System.out.println();
-
-        // Obtain the Request Token
-        System.out.println("Fetching the Request Token...");
-        Token requestToken = service.getRequestToken();
-        System.out.println("Got the Request Token!");
-        System.out.println();
-
-        System.out.println("Now go and authorize Scribe here:");
-        System.out.println(service.getAuthorizationUrl(requestToken));
-        System.out.println("And paste the verifier here");
-        System.out.print(">>");
-        Verifier verifier = new Verifier(in.nextLine());
-        System.out.println();
-
-        // Trade the Request Token and Verfier for the Access Token
-        System.out.println("Trading the Request Token for an Access Token...");
-        Token accessToken = service.getAccessToken(requestToken, verifier);
-        System.out.println("Got the Access Token!");
-        System.out.println("(if you're curious, it looks like this: " + accessToken + " )");
-        System.out.println();
-
-        // Now let's go and ask for a protected resource!
-        System.out.println("Now we're going to access a protected resource...");
-        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
-        service.signRequest(accessToken, request);
-        Response response = request.send();
-        System.out.println("Got it! Lets see what we found...");
-        System.out.println();
-        System.out.println(response.getBody());
-
-        System.out.println();
-        System.out.println("That's it man! Go and build something awesome with Scribe! :)");
-    }
-
-
-
-    public static void mainx(String[] args) {
-        Main main = new Main();
-        System.out.println("Main start");
-        //main.run(args[0], args[1], args[2], args[3]);
-        //main.testRun(args[0], args[1]);
-        main.run2(args[0], args[1]);
-
-
     }
 
     public static void main(String[] args) throws Exception {
