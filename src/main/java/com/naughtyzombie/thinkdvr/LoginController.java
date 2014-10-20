23e61f833f288b6aec6e48a4fbf1a2e2c3731d3e
Pram Attale <pram@attale.com>
2014-10-20 22:24:00 +0100

    adding screen

---
+++ src/main/java/com/naughtyzombie/thinkdvr/LoginController.java
@@ -0,0 +1,18 @@
+package com.naughtyzombie.thinkdvr;/**
+ * Created by pram on 20/10/2014.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
