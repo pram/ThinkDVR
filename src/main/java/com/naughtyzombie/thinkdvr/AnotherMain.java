2f91c2d51305f23283aa64f9c52d54700a679790
pram <pram@attale.com>
2014-11-04 22:06:34 +0000

    Adding toggle button

--- src/main/java/com/naughtyzombie/thinkdvr/AnotherMain.java
+++ src/main/java/com/naughtyzombie/thinkdvr/AnotherMain.java
@@ -1,11 +1,13 @@
 package com.naughtyzombie.thinkdvr;
 
+import com.naughtyzombie.thinkdvr.control.ThinkDVRPopover;
 import com.naughtyzombie.thinkdvr.control.ThinkDVRToolbar;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -19,6 +21,7 @@ public class AnotherMain extends Application {
     private Pane root;
     private ThinkDVRToolbar toolBar;
     private ToggleButton listButton;
+    private ThinkDVRPopover popover;
 
     @Override
     public void init() throws Exception {
@@ -28,10 +31,33 @@ public class AnotherMain extends Application {
                 super.layoutChildren();
                 final double w = getWidth();
                 final double h = getHeight();
+                popover.autosize();
                 final double toolBarHeight = toolBar.prefHeight(w);
 
             }
         };
+
+        toolBar = new ThinkDVRToolbar();
+        root.getChildren().add(toolBar);
+
+        listButton = new ToggleButton();
+        HBox.setMargin(listButton, new Insets(0, 0, 0, 7));
+
+        popover = new ThinkDVRPopover();
+        popover.setPrefWidth(440);
+        root.getChildren().add(popover);
+
+        listButton.setOnMouseClicked((MouseEvent e) -> {
+            if (popover.isVisible()) {
+                popover.hide();
+            } else {
+                popover.clearPages();
+                //popover.pushPage(rootPage);
+                popover.show(() -> listButton.setSelected(false));
+            }
+        });
+
+        toolBar.addLeftItems(listButton);
     }
 
     @Override
