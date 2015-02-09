38cdf0572c79e24f6cd19d937165321a3cdb1cb8
pram <pram@attale.com>
2015-02-09 21:30:24 +0000

    compiling - almost

---
+++ src/main/resources/web/js/thinkdvr.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('thinkdvr', ['uiGmapgoogle-maps']);
+
+app.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+});
+
+app.controller("someController", function($scope, uiGmapGoogleMapApi) {
+    // Do stuff with your $scope.
+    // Note: Some of the directives require at least something to be defined originally!
+    // e.g. $scope.markers = []
+
+    // uiGmapGoogleMapApi is a promise.
+    // The "then" callback function provides the google.maps object.
+    uiGmapGoogleMapApi.then(function(maps) {
+
+    });
+});
