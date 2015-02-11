980d13fc43c7786e90426e1e2f26164e31cfb70b
pram <pram@attale.com>
2015-02-11 20:31:00 +0000

    Map now appearing

--- src/main/resources/web/js/thinkdvr.js
+++ src/main/resources/web/js/thinkdvr.js
@@ -5,20 +5,23 @@ var app = angular.module('thinkdvr', ['uiGmapgoogle-maps']);
 
 app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
+    //    key: 'your api key',
         v: '3.17',
         libraries: 'weather,geometry,visualization'
     });
 });
 
-app.controller("someController", function($scope, uiGmapGoogleMapApi) {
+app.controller("mainMapController", function($scope, uiGmapGoogleMapApi) {
     // Do stuff with your $scope.
     // Note: Some of the directives require at least something to be defined originally!
     // e.g. $scope.markers = []
 
+    $scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 3 };
+
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
     uiGmapGoogleMapApi.then(function(maps) {
+        //$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
     });
 });
