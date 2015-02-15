62103a759984e5db500e589e14e356743e0abb2c
pram <pram@attale.com>
2015-02-15 18:05:45 +0000

    Working map representation

--- src/main/resources/web/js/thinkdvr.js
+++ src/main/resources/web/js/thinkdvr.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('thinkdvr', ['uiGmapgoogle-maps']);
+var app = angular.module('thinkdvr', ['uiGmapgoogle-maps','ngMaterial']);
 
 app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-    //    key: 'your api key',
+    key: '',
         v: '3.17',
         libraries: 'weather,geometry,visualization'
     });
@@ -15,8 +15,26 @@ app.controller("mainMapController", function($scope, uiGmapGoogleMapApi) {
     // Do stuff with your $scope.
     // Note: Some of the directives require at least something to be defined originally!
     // e.g. $scope.markers = []
+    var vm = this;
 
-    $scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 3 };
+    //$scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 3 };
+
+    vm.map = {
+        center:{
+            latitude: 51.284197,
+            longitude: -0.741352
+        },
+        zoom: 12
+    };
+
+    vm.places = [
+                 {
+                   idKey: 583187,
+                   latitude: 51.284197,
+                   longitude: -0.741352,
+                   title: "title"
+                 }
+               ];
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
