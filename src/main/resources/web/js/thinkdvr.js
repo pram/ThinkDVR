fd2de37728f5395da446f461d52cda94ff0df2f6
pram <pram@attale.com>
2015-05-28 09:36:05 +0100

    updating amended file

--- src/main/resources/web/js/thinkdvr.js
+++ src/main/resources/web/js/thinkdvr.js
@@ -42,3 +42,16 @@ app.controller("mainMapController", function($scope, uiGmapGoogleMapApi) {
 
     });
 });
+
+app.controller('BookController', ['$scope', function($scope) {  
+    $scope.book = {
+        id: 1,
+        name: 'Harry Potter',
+        author: 'J. K. Rowling',
+        stores: [
+            { id: 1, name: 'Barnes & Noble', quantity: 3},
+            { id: 2, name: 'Waterstones', quantity: 2},
+            { id: 3, name: 'Book Depository', quantity: 5}
+        ]
+    };
+}]);
